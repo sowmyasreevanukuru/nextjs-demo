046f5c2dc4e03ddb55afdb8089dcc4c670f10224
Sowmyasree Vanukuru <96767583+sowmyasreevanukuru@users.noreply.github.com>
2022-11-10 17:59:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-import MeetupList from "../components/meetups/MeetupList";
+import MeetupList from "../components/meetups/MeetUpList";
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
